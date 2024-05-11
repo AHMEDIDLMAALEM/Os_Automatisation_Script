1db40f51d04548040f1fd09e3aea3314ba1db89d
Haida Hossam <162696587+BOSS0x01@users.noreply.github.com>
2024-05-11 15:01:47 +0100

    Update FolderOrganizer.sh
    adding comments

--- FolderOrganizer.sh
+++ FolderOrganizer.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+#List that will hold the file names 
 FileNamesList=()
 
 CheckZenityDependence(){
     package_name="zenity"
-    
+
     #checks if the package zenity is installed
     dpkg-query -W -f='${Status}\n' $package_name | grep -q 'installed'
 
@@ -28,11 +29,11 @@ CheckZenityDependence(){
 
 PopulateListWithFileNames(){	
 
-    #checks if there is a folder provided as an argument and the folder does exist  
+    #checks if there is a folder provided as an argument and the folder does exist
     if [ $# -ge 1 ]; then
     	if [ -d "$1" ]; then
 		selected_dir=$1
-	    else
+	else
 		echo "Directory does not exist"
 		return 1
         fi
@@ -64,18 +65,24 @@ PopulateListWithFileNames(){
     return 0
 }
 
-#
+
 FileOrganizer(){
 
-
+    #Fills the list with file names 
     PopulateListWithFileNames $1
 
     if [ $? -eq 0 ];then
 
+        # That is the main loop that will loop through existing files 
+        # It will create categorized folders based on the extensions of existing files
+        # moves the file to the correspondent folder 
+
         for File in "${FileNamesList[@]}"; do
 
+            #get the extension from the file
             Extension=$(echo "$File" | sed 's/.*\.//' )
 
+            #a switch case that will categorize files and make them into a folder 
             case $Extension in 
                 "aif" | "cda" | "mid" | "midi" | "mp3" | "mpa" | "ogg" | "wav" | "wma" | "wpl" )
                     MoveFileToCorrectFolder "Audio"  "$File"
@@ -129,6 +136,8 @@ FileOrganizer(){
                     ;;
             esac 
         done
+
+        # if zenity exist use it to display an info else use the echo
         dpkg-query -W -f='${Status}\n' $package_name | grep -q 'installed'
 
 	    if [ $? -eq 0 ]; then
@@ -140,13 +149,18 @@ FileOrganizer(){
 }
 
 MoveFileToCorrectFolder(){
+
     FolderName=$1
     FileName=$2
+
+    #if the folder doesn't exist create a new one
     if [ ! -d "$FolderName" ]; then
         mkdir "$FolderName" 
     fi
+
+    #move the file to the folder
     mv "$FileName" "$FolderName/"
 }
-echo "Starting the script $1"
-FileOrganizer $1
 
+#calling the main function that takes a path as an argument 
+FileOrganizer $1
